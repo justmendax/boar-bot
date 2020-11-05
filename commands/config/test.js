02e38605d9b29e1e156b21a844aeb6232ac6164e
Mendax <notjustmendax@gmail.com>
2020-11-05 19:12:55 +0200

    Delete test.js

--- commands/config/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "test",
-    category: "config",
-    description: "Test",
-    run: async (client, message, args) => {
-        message.channel.send(message.guild.members.fetch({ query: "luna", limit: 1 }).displayName);
-    }
-}
