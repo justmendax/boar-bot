92bad32d4446543e91759cf53af823fedcd7923f
Mendax <cosmin.gamer7@gmail.com>
2020-11-29 01:23:01 +0200

    O Navagos. :boar:

---
+++ commands/utils/navagos.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+const { hostGuild } = require("../../bot.js");
+
+module.exports = {
+    name: "navagos",
+    category: "utils",
+    description: "Shows a list of all Navagos playlists.",
+    run: async (client, message, args) => {
+        const list = [["Atrocious Infinity and Sons", "https://www.youtube.com/playlist?list=PLzqWn1iG-DYsfXIvuLwI3gF2kutMrYFWd"], ["Atrocious Infinity and Sons II", "https://www.youtube.com/playlist?list=PLrmsk9amz4tY1tG4k3AeXSbbBl9oicdyS"], ["THE MOUTH - Uplifting Force I", "https://www.youtube.com/watch?v=teU-ZhfNLfE"]];
+        const embed = new MessageEmbed()
+            .setColor(client.guilds.cache.get(hostGuild).me.displayHexColor)
+            .setTimestamp()
+            .setAuthor(message.author.tag, message.author.avatarURL())
+            .setFooter(client.user.username, client.user.avatarURL())
+            .setTitle("Navagos Playlists");
+        
+        list.forEach((arr) => {
+            embed.addField(arr[0], arr[1]);
+        });
+
+        message.channel.send(embed);
+    }
+}
