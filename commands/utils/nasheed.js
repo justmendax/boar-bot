82da0871047396691b932e4b802ffbb349a0e9a1
Mendax <cosmin.gamer7@gmail.com>
2020-11-29 23:00:20 +0200

    Added Nasheed. :boar:

--- commands/utils/nasheed.js
+++ commands/utils/nasheed.js
@@ -2,19 +2,19 @@ const { MessageEmbed } = require("discord.js");
 const { hostGuild } = require("../../bot.js");
 
 module.exports = {
-    name: "navagos",
+    name: "nasheed",
     category: "utils",
-    description: "Shows a list of all Navagos playlists.",
+    description: "Shows a list of all Nasheed songs.",
     run: async (client, message, args) => {
-        const list = [["Atrocious Infinity and Sons", "https://www.youtube.com/playlist?list=PLzqWn1iG-DYsfXIvuLwI3gF2kutMrYFWd"], 
-        ["Atrocious Infinity and Sons II", "https://www.youtube.com/playlist?list=PLrmsk9amz4tY1tG4k3AeXSbbBl9oicdyS"], 
-        ["THE MOUTH - Uplifting Force I", "https://www.youtube.com/watch?v=teU-ZhfNLfE"]];
+        const list = [["Kun Faya Kun Full Video Song Rockstar | Ranbir Kapoor | A.R. Rahman, Javed Ali, Mohit Chauhan", "https://www.youtube.com/watch?v=T94PHkuydcw"],
+        ["Naat Qaseeda Molana Anas Younus Mere Aqa Mery Mery Mola", "https://www.youtube.com/watch?v=jhGz2DUcKIo"],
+        ["Maula Ya Salli Ft. Sami Yusuf Qasida Burda Shareef [NASHEED]", "https://www.youtube.com/watch?v=fvIWPFEZi_s"]];
         const embed = new MessageEmbed()
             .setColor(client.guilds.cache.get(hostGuild).me.displayHexColor)
             .setTimestamp()
             .setAuthor(message.author.tag, message.author.avatarURL())
             .setFooter(client.user.username, client.user.avatarURL())
-            .setTitle("Navagos Playlists");
+            .setTitle("Nasheed Songs");
         
         list.forEach((arr) => {
             embed.addField(arr[0], arr[1]);
