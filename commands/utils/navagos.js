05ad17d15243fb65247dcaead3625cb8630ad32f
Mendax <cosmin.gamer7@gmail.com>
2021-04-24 00:18:04 +0300

    Navagos :boar:

--- commands/utils/navagos.js
+++ commands/utils/navagos.js
@@ -12,6 +12,7 @@ module.exports = {
 	["THE MOUTH - Uplifting Force II (Blue Version)", "https://www.youtube.com/watch?v=KgEALk44pbg"],
 	["THE MOUTH - Uplifting Force II (Red Version)", "https://www.youtube.com/watch?v=teQPo-vH7kI"],
 	[`THE MOUTH - Uplifting Force II (Black "End-All" Version)`, "https://www.youtube.com/watch?v=aVDAaol8HEg"],
+	["THE MOUTH - Uplifting Force III (Anthems of Destiny)", "https://www.youtube.com/watch?v=dnxEKE2xF84"],
 	["THE MOUTH - Cats and Mouse (SAPP/RL)", "https://www.youtube.com/watch?v=5bsI-L0MOEM"],
 	["THE MOUTH - Heroes of Faith I", "https://www.youtube.com/watch?v=ORY_iWoLreY"],
 	["Dunkler und anderer Berlin", "https://www.youtube.com/playlist?list=PLrmsk9amz4tbqvLScDApU9xdn_s8O3UZU"]];
