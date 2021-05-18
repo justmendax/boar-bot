6e3aba77affa0ecdaaa0b5a9f6d30118378eae6b
Mendax <cosmin.gamer7@gmail.com>
2021-05-18 13:35:14 +0300

    Navagos. :boar:

--- commands/utils/navagos.js
+++ commands/utils/navagos.js
@@ -7,7 +7,8 @@ module.exports = {
     description: "Shows a list of all Navagos playlists :boar:",
     run: async (client, message, args) => {
         const list = [["Atrocious Infinity and Sons", "https://www.youtube.com/playlist?list=PLzqWn1iG-DYsfXIvuLwI3gF2kutMrYFWd"], 
-        ["Atrocious Infinity and Sons II", "https://www.youtube.com/playlist?list=PLrmsk9amz4tY1tG4k3AeXSbbBl9oicdyS"], 
+        ["Atrocious Infinity and Sons II", "https://www.youtube.com/playlist?list=PLrmsk9amz4tY1tG4k3AeXSbbBl9oicdyS"],
+	["Dunkler und anderer Berlin", "https://www.youtube.com/playlist?list=PLrmsk9amz4tbqvLScDApU9xdn_s8O3UZU"], 
         ["Uplifting Force I", "https://www.youtube.com/watch?v=teU-ZhfNLfE"],
 	["Uplifting Force II (Blue Version)", "https://www.youtube.com/watch?v=KgEALk44pbg"],
 	["Uplifting Force II (Red Version)", "https://www.youtube.com/watch?v=teQPo-vH7kI"],
@@ -15,10 +16,10 @@ module.exports = {
 	["Uplifting Force III (Anthems of Destiny)", "https://www.youtube.com/watch?v=dnxEKE2xF84"],
 	["Uplifting Force IV (Scroll of Resurrection)", "https://www.youtube.com/watch?v=VqED6GiTJJM"],
 	["Uplifting Force V (State of Transition)", "https://www.youtube.com/watch?v=0gMr-bqwI0k"],
-	["War Thunder: No Limit", "https://www.youtube.com/watch?v=iZw5WggrKrs"],
-	["Cats and Mouse (SAPP/RL)", "https://www.youtube.com/watch?v=5bsI-L0MOEM"],
 	["Heroes of Faith I", "https://www.youtube.com/watch?v=ORY_iWoLreY"],
-	["Dunkler und anderer Berlin", "https://www.youtube.com/playlist?list=PLrmsk9amz4tbqvLScDApU9xdn_s8O3UZU"]];
+	["War Thunder: No Limit", "https://www.youtube.com/watch?v=iZw5WggrKrs"],
+	["Sunset Drive", "https://www.youtube.com/watch?v=Mv-zG8SIhU4"],
+	["Cats and Mouse", "https://www.youtube.com/watch?v=5bsI-L0MOEM"]];
         const embed = new MessageEmbed()
             .setColor(client.guilds.cache.get(hostGuild).me.displayHexColor)
             .setTimestamp()
