f9718e0c196fb095b61782881e6418ca3746e3d3
Mendax <cosmin.gamer7@gmail.com>
2021-07-09 01:40:12 +0300

    Navagos. :boar:

--- commands/utils/navagos.js
+++ commands/utils/navagos.js
@@ -1,24 +1,37 @@
 const { MessageEmbed } = require("discord.js");
 const { hostGuild } = require("../../bot.js");
-const fs = require("fs");
+const { Client } = require("pg");
 
 module.exports = {
     name: "navagos",
     category: "utils",
     description: "Shows a list of all Navagos playlists :boar:",
     run: async (client, message, args) => {
-        const navagos = JSON.parse(fs.readFileSync("commands/utils/navagos.json"));
         const embed = new MessageEmbed()
             .setColor(client.guilds.cache.get(hostGuild).me.displayHexColor)
             .setTimestamp()
             .setAuthor(message.author.tag, message.author.avatarURL())
             .setFooter(client.user.username, client.user.avatarURL())
             .setTitle("Navagos Playlists");
-        
-        navagos.playlists.forEach((obj) => {
-            embed.addField(`${navagos.playlists.indexOf(obj) + 1}. ${obj.title}`, obj.url);
+
+        const db = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
         });
 
+        db.connect();
+
+        db.query('SELECT * FROM playlists', (err, res) => {
+            if(err)
+                throw err;
+            else {
+                res.rows.forEach(row => embed.addField(`${row.id}. ${row.title}`, row.url));
+            }
+        });
+
+        db.end();
         message.channel.send(embed);
     }
 }
